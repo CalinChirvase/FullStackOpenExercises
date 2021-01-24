719dd479f88ea28f61daf9ef64c0db57d79b865b
CalinChirvase <calin.chirvase@yahoo.com>
2021-01-23 20:12:42 -0500

    Add exercises 4.15-4.19

--- part4/app.js
+++ part4/app.js
@@ -4,9 +4,11 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blog')
+const userRouter = require('./controllers/user')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -21,9 +23,11 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-app.use(middleware.requestLogger)
 
+app.use(middleware.requestLogger)
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,28 +1,16 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
-const initialBlogs = [
-    {
-        title: 'The Best Blog',
-        author: 'Blogger McBlogg',
-        url: 'thatblog.com',
-        likes: 9001
-    },
-    {
-        title: 'The Real Best Blog',
-        author: 'Blogger OBlogger',
-        url: 'thatrealblog.com',
-        likes: 5
-    }
-]
+
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
+    let blogObject = new Blog(helper.initialBlogs[0])
     await blogObject.save()
-    blogObject = new Blog(initialBlogs[1])
+    blogObject = new Blog(helper.initialBlogs[1])
     await blogObject.save()
 })
 
@@ -35,7 +23,7 @@ test('blogs are returned as json', async () => {
 
 test('all blogs are returned', async () => {
     const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(initialBlogs.length)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
 test('a specific blog is within the returned blogs', async () => {
@@ -60,12 +48,13 @@ test('a valid blog can be added', async () => {
         .send(newBlog)
         .expect(200)
         .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
   
-    const response = await api.get('/api/blogs')
+    const titles = blogsAtEnd.body.map(r => r.title)
   
-    const titles = response.body.map(r => r.title)
-  
-    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(titles).toHaveLength(helper.initialBlogs.length + 1)
     expect(titles).toContain(
         'Hello World!'
     )
@@ -82,10 +71,9 @@ test('blog without author is not added', async () => {
         .post('/api/blogs')
         .send(newBlog)
         .expect(400)
-  
-    const response = await api.get('/api/blogs')
-  
-    expect(response.body).toHaveLength(initialBlogs.length)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
 })
 
 test('blog without title and url is not added', async () => {
@@ -99,9 +87,8 @@ test('blog without title and url is not added', async () => {
         .send(newBlog)
         .expect(400)
   
-    const response = await api.get('/api/blogs')
-  
-    expect(response.body).toHaveLength(initialBlogs.length)
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
 })
 
 test('blog with no likes defaults to 0 likes', async () => {
