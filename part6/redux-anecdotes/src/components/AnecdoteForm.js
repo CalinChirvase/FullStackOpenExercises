651825f47019a142c91781925183f3922d79736b
CalinChirvase <calin.chirvase@yahoo.com>
2021-02-03 10:17:08 -0500

    Add exercises 6.3-6.14

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification, removeNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const create = (event) => {
+  const create = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.createNewAnecdote(content)
+    dispatch(createAnecdote(newAnecdote))
     dispatch(setNotification('you created: '.concat(content)))
     setTimeout(() => dispatch(removeNotification()), 5000)
 
