4be9c50d193d85474a4b56dd6b43502798f3f4cf
CalinChirvase <calin.chirvase@yahoo.com>
2021-02-11 15:50:28 -0500

    Add exercises 9.1-9.3

---
+++ part9/calculators/bmiCalculator.ts
@@ -0,0 +1,20 @@
+type BMI = 'Under' | 'Normal (healthy weight)' | 'Over' | 'Obese';
+const calculateBmi = (height: number, mass: number): BMI => {
+    //convert height from cm to m when calculating bmi
+    const bmi: number = mass / (height/100)**2;
+    if (bmi < 18.5) {
+        return 'Under';
+    } else if (18.5 <= bmi && bmi <= 25) {
+        return 'Normal (healthy weight)';
+    } else if (25 < bmi && bmi <= 30) {
+        return 'Over';
+    } else if (bmi > 30) {
+        return 'Obese';
+    }
+}
+
+if (process.argv.length !== 4) {
+    throw Error('call calculateBmi with two parameters, height in cm and mass in kg')
+}
+
+console.log(calculateBmi(parseInt(process.argv[2]), parseInt(process.argv[3])));
---
+++ part9/calculators/exerciseCalculator.ts
@@ -0,0 +1,59 @@
+interface Result {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+};
+
+const giveRating = (average: number, target: number) : number => {
+    if (average <= target * 0.5) {
+        return 1;
+    } else if (average < target) {
+        return 2;
+    } else if (average === target) {
+        return 3;
+    } else if (average >= target * 1.5) {
+        return 4;
+    } else {
+        return 5;
+    }
+}
+const getRatingDescription = (rating: number): string => {
+    switch(rating) {
+        case 1:
+            return 'you\'re not doing well';
+        case 2:
+            return 'not too bad but could be better';
+        case 3:
+            return 'you\'ve hit your target';
+        case 4:
+            return 'you\'ve surpassed you\'re goal';
+        case 5:
+            return 'you are going far above you\'re intended target, congratulations!';
+        default:
+            throw new Error('getRatingDescription received improper argument!');
+    }
+}
+const calculateExercise = (trainingSchedule: Array<number>, target: number): Result => {
+    const average: number = trainingSchedule.reduce((a, b) => a + b, 0) / trainingSchedule.length;
+    const rating: number = giveRating(average, target);
+    return {
+        periodLength: trainingSchedule.length,
+        trainingDays: trainingSchedule.filter(hours => hours !== 0).length,
+        success: average >= target,
+        rating: rating,
+        ratingDescription: getRatingDescription(rating),
+        target: target,
+        average: average
+    };
+}
+
+if (process.argv.length !== 4) {
+    throw Error('call calculateExercise with two parameters, list of daily hours exercised(list of numbers must not contain spaces) and target hours')
+}
+//convert user inputed string into a list of numbers
+const listInputToNumbers: Array<number> = process.argv[2].replace('[', '').replace(']', '').split(',').map(hour => parseInt(hour));
+console.log(calculateExercise(listInputToNumbers, parseInt(process.argv[3])));
