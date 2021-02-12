e5879af04926aaba6c2d0052faf743dd01329b0c
CalinChirvase <calin.chirvase@yahoo.com>
2021-02-12 11:46:53 -0500

    Add exercises 9.12-9.13

--- part9/patientor-backend/src/services/patientService.ts
+++ part9/patientor-backend/src/services/patientService.ts
@@ -1,7 +1,8 @@
-import patientData from '../../data/patients.json';
-import { Patient, NonSensitivePatientEntry } from '../types';
+import patientsData from '../../data/patients';
+import { Patient, NonSensitivePatientEntry, NewPatientEntry } from '../types';
+import { v4 as uuid } from 'uuid';
 
-const patients: Array<Patient> = patientData;
+const patients: Array<Patient> = patientsData;
 
 const getNonSensitiveEntries = (): NonSensitivePatientEntry[] => {
     return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
@@ -13,11 +14,16 @@ const getNonSensitiveEntries = (): NonSensitivePatientEntry[] => {
     }));
 };
 
-const addEntry = (): null => {
-    return null;
+const addPatient = (entry: NewPatientEntry): Patient => {
+    const newPatientEntry = {
+        id: uuid(),
+        ...entry
+    };
+    patients.push(newPatientEntry);
+    return newPatientEntry;
 };
 
 export default {
     getNonSensitiveEntries,
-    addEntry
+    addPatient
 };
--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,16 +1,23 @@
+export enum Gender {
+    Male = "male",
+    Female = "female",
+    Other = "other"
+}
+
 export interface Diagnosis {
     code : string,
     name: string,
     latin?: string
-};
+}
 
 export interface Patient {
     id: string,
     name: string,
     dateOfBirth: string,
     ssn: string,
-    gender: string,
+    gender: Gender,
     occupation: string
-};
+}
 
 export type NonSensitivePatientEntry = Omit<Patient, 'ssn'>;
+export type NewPatientEntry = Omit<Patient, 'id'>;
